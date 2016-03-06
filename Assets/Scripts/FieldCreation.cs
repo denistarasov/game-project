3b79996006cd2e4c24e2106d9b5dd108bdc18092
Denis Tarasov <denistarasov@users.noreply.github.com>
2016-03-06 18:00:31 +0300

    Dicilization v.0.1
    + now the player can conquer any area he'd like

--- Assets/Scripts/FieldCreation.cs
+++ Assets/Scripts/FieldCreation.cs
@@ -2,18 +2,23 @@
 using System.Collections;
 
 public class FieldCreation : MonoBehaviour {
-
 	// Объявление глобальных константных переменных
 	// Количество клеток по горизонтали и вертикали
 	public static int number_cells_x = 28;
 	public static int number_cells_y = 10;
 
 	// Сдвиг поля от (0;0)
-	public static int shift_horizontally = 5;
-	public static int shift_vertically = 9;
+	public static int shift_horizontally = 0;
+	public static int shift_vertically = 0;
 
 	// Размер одной клетки
 	public static int cell_size = 30;
+
+	// Разрешение будет 1024х768, соответственно делаем
+	// размеры и количество клеток
+	// Но пока сделаем его зависящим от размера клеток, а не наоборот
+	public static int res_horizontal = cell_size * number_cells_x;
+	public static int res_vertical = cell_size * number_cells_y;
 
 	// Создадим кнопки разных цветов
 	private Texture square_button;
@@ -40,6 +45,7 @@ public class FieldCreation : MonoBehaviour {
 	public static int[] belonging_to_player = new int[] {1, 0, 0, 0, 0, 0, 0, 2};
 
 	void Start () {
+		Screen.SetResolution(res_horizontal, res_vertical, false);
 		white_square = (Texture)Resources.Load("white_square");
 		green_square = (Texture)Resources.Load("green_square");
 		red_square = (Texture)Resources.Load("red_square");
--- Assets/Scripts/MouseClick.cs
+++ Assets/Scripts/MouseClick.cs
@@ -7,35 +7,32 @@ public class MouseClick : MonoBehaviour {
 
 	void Update () {
 		if (Input.GetButtonDown("Fire1")) {
+
 			// Находим место, куда кликнул игрок, точка отсчета -- левый нижний угол
 			click_coordinates = Input.mousePosition;
 
-			/*// Переведем это значение в систему координат с отсчетом в левом верхнем углу
-			click_coordinates.y -= Screen.height;
-			Debug.Log ("click_coord x == " + click_coordinates.x.ToString());
-			Debug.Log ("click_coord y == " + click_coordinates.y.ToString());
-
+			// Переведем это значение в систему координат с отсчетом в левом верхнем углу
+			click_coordinates.y = Screen.height - click_coordinates.y;
 
 			// Учитываем сдвиг самого поля (сдвиг для выравнивания по центру)
 			click_coordinates.x += FieldCreation.shift_horizontally;
 			click_coordinates.y -= FieldCreation.shift_vertically;
-			*/
 
 			// НАДО ПЕРЕДЕЛАТЬ УЧИТЫВАЯ НАЧАЛА КООРДИНАТ В ЛЕВОМ НИЖНЕМ УГЛУ
 
 			// Получаем координаты клетки, куда кликнул игрок
 			click_coordinates.x = click_coordinates.x / FieldCreation.cell_size;
 			click_coordinates.y = click_coordinates.y / FieldCreation.cell_size;
-			int coordinates_in_array = (int)click_coordinates.y * FieldCreation.number_cells_y + (int)click_coordinates.x;
+
+			// А теперь эти же координаты -- но в массиве клеток поля
+			int coordinates_in_array = (int)click_coordinates.y * FieldCreation.number_cells_x + (int)click_coordinates.x;
 
 			// Ищем зону
-			int zone_number = FieldCreation.belonging_to_player [FieldCreation.array_of_hexagons [coordinates_in_array] ];
+			int zone_number = FieldCreation.array_of_hexagons [coordinates_in_array];
+			int owner_number = FieldCreation.belonging_to_player [FieldCreation.array_of_hexagons [coordinates_in_array] ];
+			
 			// Теперь ее владелец -- игрок
 			FieldCreation.belonging_to_player [zone_number] = 1;
-
-			Debug.Log ("zone_number == " + zone_number.ToString ());
-
-
 		}
 	}
 }
