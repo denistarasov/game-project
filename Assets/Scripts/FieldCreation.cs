5e5b2995d4339f6f7659e2c8dea297b42dde7990
Denis Tarasov <denistarasov@users.noreply.github.com>
2016-03-08 00:39:45 +0300

    Converted tabs to spaces and deleted old script
    Converted tabs to spaces and deleted Old_Main.cs

--- Assets/Scripts/FieldCreation.cs
+++ Assets/Scripts/FieldCreation.cs
@@ -2,66 +2,66 @@
 using System.Collections;
 
 public class FieldCreation : MonoBehaviour {
-	// Объявление глобальных константных переменных
-	// Количество клеток по горизонтали и вертикали
-	public static int number_cells_x = 28;
-	public static int number_cells_y = 10;
+    // Объявление глобальных константных переменных
+    // Количество клеток по горизонтали и вертикали
+    public static int number_cells_x = 28;
+    public static int number_cells_y = 10;
 
-	// Сдвиг поля от (0;0)
-	public static int shift_horizontally = 0;
-	public static int shift_vertically = 0;
+    // Сдвиг поля от (0;0)
+    public static int shift_horizontally = 0;
+    public static int shift_vertically = 0;
 
-	// Размер одной клетки
-	public static int cell_size = 30;
+    // Размер одной клетки
+    public static int cell_size = 30;
 
-	// Разрешение будет 1024х768, соответственно делаем
-	// размеры и количество клеток
-	// Но пока сделаем его зависящим от размера клеток, а не наоборот
-	public static int res_horizontal = cell_size * number_cells_x;
-	public static int res_vertical = cell_size * number_cells_y;
+    // Разрешение будет 1024х768, соответственно делаем
+    // размеры и количество клеток
+    // Но пока сделаем его зависящим от размера клеток, а не наоборот
+    public static int res_horizontal = cell_size * number_cells_x;
+    public static int res_vertical = cell_size * number_cells_y;
 
-	// Создадим кнопки разных цветов
-	private Texture square_button;
-	private Texture red_square;
-	private Texture green_square;
-	private Texture white_square;
+    // Создадим кнопки разных цветов
+    private Texture square_button;
+    private Texture red_square;
+    private Texture green_square;
+    private Texture white_square;
 
-	// Создаем матрицу кнопок, сверху которых будут спрайты-шестиугольники.
-	// Каждая ячейка матрицы обозначает принадлежность к той или иной "зоне".
-	// Зона -- это то, что может захватывать игрок
-	public static int[] array_of_hexagons = new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3,
-												  		0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3,
-												  		0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3,
-												  		0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3,
-												  		0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3,
-												  		4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7,
-												  		4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7,
-												  		4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7,
-												  		4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7,
-												  		4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7 };
-	// Теперь создадим массив,
-	// индексы -- это зоны,
-	// элементы -- принадлежность конкретному игроку
-	public static int[] belonging_to_player = new int[] {1, 0, 0, 0, 0, 0, 0, 2};
+    // Создаем матрицу кнопок, сверху которых будут спрайты-шестиугольники.
+    // Каждая ячейка матрицы обозначает принадлежность к той или иной "зоне".
+    // Зона -- это то, что может захватывать игрок
+    public static int[] array_of_hexagons = new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3,
+                                                        0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3,
+                                                        0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3,
+                                                        0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3,
+                                                        0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3,
+                                                        4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7,
+                                                        4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7,
+                                                        4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7,
+                                                        4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7,
+                                                        4, 4, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7 };
+    // Теперь создадим массив,
+    // индексы -- это зоны,
+    // элементы -- принадлежность конкретному игроку
+    public static int[] belonging_to_player = new int[] {1, 0, 0, 0, 0, 0, 0, 2};
 
-	void Start () {
-		Screen.SetResolution(res_horizontal, res_vertical, false);
-		white_square = (Texture)Resources.Load("white_square");
-		green_square = (Texture)Resources.Load("green_square");
-		red_square = (Texture)Resources.Load("red_square");
-	}
+    void Start () {
+        Screen.SetResolution(res_horizontal, res_vertical, false);
+        white_square = (Texture)Resources.Load("white_square");
+        green_square = (Texture)Resources.Load("green_square");
+        red_square = (Texture)Resources.Load("red_square");
+    }
 
-	void OnGUI () {
-		// Выводим кнопки-шестиугольники с неким константным сдвигом cell_size
-		for (int i = 0; i < 280; ++i) {
-			square_button = white_square;
-			if (belonging_to_player [array_of_hexagons [i]] == 1)
-				square_button = red_square;
-			if (belonging_to_player [array_of_hexagons [i]] == 2)
-				square_button = green_square;
-			
-			// Rect (horizontal, vertical, width, height)
-			GUI.DrawTexture (new Rect (shift_horizontally + cell_size * (i % number_cells_x), shift_vertically + cell_size * (i / number_cells_x), cell_size, cell_size), square_button);
-		}
-	}
+    void OnGUI () {
+        // Выводим кнопки-шестиугольники с неким константным сдвигом cell_size
+        for (int i = 0; i < 280; ++i) {
+            square_button = white_square;
+            if (belonging_to_player [array_of_hexagons [i]] == 1)
+                square_button = red_square;
+            if (belonging_to_player [array_of_hexagons [i]] == 2)
+                square_button = green_square;
+            
+            // Rect (horizontal, vertical, width, height)
+            GUI.DrawTexture (new Rect (shift_horizontally + cell_size * (i % number_cells_x), shift_vertically + cell_size * (i / number_cells_x), cell_size, cell_size), square_button);
+        }
+    }
 }
--- Assets/Scripts/MouseClick.cs
+++ Assets/Scripts/MouseClick.cs
@@ -3,36 +3,34 @@ using System.Collections;
 
 public class MouseClick : MonoBehaviour {
 
-	private Vector2 click_coordinates;
+    private Vector2 click_coordinates;
 
-	void Update () {
-		if (Input.GetButtonDown("Fire1")) {
+    void Update () {
+        if (Input.GetButtonDown("Fire1")) {
 
-			// Находим место, куда кликнул игрок, точка отсчета -- левый нижний угол
-			click_coordinates = Input.mousePosition;
+            // Находим место, куда кликнул игрок, точка отсчета -- левый нижний угол
+            click_coordinates = Input.mousePosition;
 
-			// Переведем это значение в систему координат с отсчетом в левом верхнем углу
-			click_coordinates.y = Screen.height - click_coordinates.y;
+            // Переведем это значение в систему координат с отсчетом в левом верхнем углу
+            click_coordinates.y = Screen.height - click_coordinates.y;
 
-			// Учитываем сдвиг самого поля (сдвиг для выравнивания по центру)
-			click_coordinates.x += FieldCreation.shift_horizontally;
-			click_coordinates.y -= FieldCreation.shift_vertically;
+            // Учитываем сдвиг самого поля (сдвиг для выравнивания по центру)
+            click_coordinates.x += FieldCreation.shift_horizontally;
+            click_coordinates.y -= FieldCreation.shift_vertically;
 
-			// НАДО ПЕРЕДЕЛАТЬ УЧИТЫВАЯ НАЧАЛА КООРДИНАТ В ЛЕВОМ НИЖНЕМ УГЛУ
+            // Получаем координаты клетки, куда кликнул игрок
+            click_coordinates.x = click_coordinates.x / FieldCreation.cell_size;
+            click_coordinates.y = click_coordinates.y / FieldCreation.cell_size;
 
-			// Получаем координаты клетки, куда кликнул игрок
-			click_coordinates.x = click_coordinates.x / FieldCreation.cell_size;
-			click_coordinates.y = click_coordinates.y / FieldCreation.cell_size;
+            // А теперь эти же координаты -- но в массиве клеток поля
+            int coordinates_in_array = (int)click_coordinates.y * FieldCreation.number_cells_x + (int)click_coordinates.x;
 
-			// А теперь эти же координаты -- но в массиве клеток поля
-			int coordinates_in_array = (int)click_coordinates.y * FieldCreation.number_cells_x + (int)click_coordinates.x;
-
-			// Ищем зону
-			int zone_number = FieldCreation.array_of_hexagons [coordinates_in_array];
-			int owner_number = FieldCreation.belonging_to_player [FieldCreation.array_of_hexagons [coordinates_in_array] ];
-			
-			// Теперь ее владелец -- игрок
-			FieldCreation.belonging_to_player [zone_number] = 1;
-		}
-	}
+            // Ищем зону
+            int zone_number = FieldCreation.array_of_hexagons [coordinates_in_array];
+            int owner_number = FieldCreation.belonging_to_player [FieldCreation.array_of_hexagons [coordinates_in_array] ];
+            
+            // Теперь ее владелец -- игрок
+            FieldCreation.belonging_to_player [zone_number] = 1;
+        }
+    }
 }
